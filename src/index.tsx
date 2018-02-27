3a436538355161b08f56ab4a007372b8e8fbdb3c
Mateusz <mat@citrusad.com>
2018-02-27 17:27:30 +1000

    enabled material-ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-//import 'styles.scss'
+import Button from 'material-ui/Button';
 
 const App = () => {
   return (
     <div className="test">
+      <Button>Test</Button>
       <p>Hello world!</p>
     </div>
   );
 };
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
