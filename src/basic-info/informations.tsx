0fb7661bdd795215e309d4af7c13a802d8159590
SirWojtek <momatoku@gmail.com>
2018-03-07 10:19:53 +1000

    contact component

--- src/basic-info/informations.tsx
+++ src/basic-info/informations.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
-import { Avatar, Icon } from 'material-ui';
+import { Avatar, Icon, List } from 'material-ui';
+import { ListItem, ListItemText } from 'material-ui/List';
 import PhoneAndroid from 'material-ui-icons/PhoneAndroid';
 import { withStyles, WithStyles, StyleRulesCallback } from 'material-ui/styles';
 import { green, pink } from 'material-ui/colors';
@@ -43,12 +44,20 @@ class Informations extends React.Component<IProps & WithStyles<ClassNames>> {
 
     return (
       <div>
-        <Avatar className={props.classes.phoneAvatar}>
-          <Icon className={props.classes.phoneIcon}>phone_android</Icon>
-        </Avatar>
-        <Avatar className={props.classes.emailAvatar}>
-          <Icon className={props.classes.emailIcon}>email</Icon>
-        </Avatar>
+        <List>
+          <ListItem>
+            <Avatar className={props.classes.phoneAvatar}>
+              <Icon className={props.classes.phoneIcon}>phone_android</Icon>
+            </Avatar>
+            <ListItemText primary="Phone" secondary={props.contact.phoneNumber}/>
+          </ListItem>
+          <ListItem>
+            <Avatar className={props.classes.emailAvatar}>
+              <Icon className={props.classes.emailIcon}>email</Icon>
+            </Avatar>
+            <ListItemText primary="Email" secondary={props.contact.email}/>
+          </ListItem>
+      </List>
       </div>
     );
   }
